50c30b6ea2c87e36d76a39ff68dead7294c9017d
ysedeveloper <ysedeveloper@gmail.com>
2020-12-12 17:59:43 +0900

    9.6. 책 수정 기능 컨트롤러 메소드 추가

--- project/src/main/java/sample/spring/yse/BookController.java
+++ project/src/main/java/sample/spring/yse/BookController.java
@@ -54,4 +54,19 @@ public class BookController {
 	    mav.setViewName("/book/update");
 	    return mav;
 	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public ModelAndView updatePost(@RequestParam Map<String, Object> map) {
+	    ModelAndView mav = new ModelAndView();
+
+	    boolean isUpdateSuccess = this.bookService.edit(map);		
+	    if (isUpdateSuccess) {
+	        String bookId = map.get("bookId").toString();
+	        mav.setViewName("redirect:/detail?bookId=" + bookId);
+	    }else {
+	        mav = this.update(map);
+	    }		
+
+	    return mav;
+	}
 }
