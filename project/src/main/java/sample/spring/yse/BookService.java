8a6e0c90df8550569cada28655311522d931df07
ysedeveloper <ysedeveloper@gmail.com>
2020-12-12 18:34:34 +0900

    11.5. 책 목록 서비스 인터페이스 메소드 시그니쳐 생성

--- project/src/main/java/sample/spring/yse/BookService.java
+++ project/src/main/java/sample/spring/yse/BookService.java
@@ -1,5 +1,6 @@
 package sample.spring.yse;
 
+import java.util.List;
 import java.util.Map;
 
 public interface BookService {
@@ -11,5 +12,7 @@ public interface BookService {
 	boolean edit(Map<String, Object> map);
 
 	boolean remove(Map<String, Object> map);
+
+	List<Map<String, Object>> list(Map<String, Object> map);
 
 }
