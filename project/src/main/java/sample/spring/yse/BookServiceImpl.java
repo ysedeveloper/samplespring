bce2a1a2ec5fc5b10bd96bc826bbba4ff9e4e7cd
ysedeveloper <ysedeveloper@gmail.com>
2020-12-12 18:34:23 +0900

    11.4. 책 목록 서비스 클래스 메소드 생성

--- project/src/main/java/sample/spring/yse/BookServiceImpl.java
+++ project/src/main/java/sample/spring/yse/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package sample.spring.yse;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,11 @@ public class BookServiceImpl implements BookService {
 	public boolean remove(Map<String, Object> map) {
 	    int affectRowCount = this.bookDao.delete(map);
 	    return affectRowCount == 1;	    
+	}
+	
+	@Override
+	public List<Map<String, Object>> list(Map<String, Object> map){
+	    return this.bookDao.selectList(map);
 	}
 	
 }
