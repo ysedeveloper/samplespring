da86b0c1559aca66ec5af8748072032fff60890e
ysedeveloper <ysedeveloper@gmail.com>
2020-12-12 18:44:55 +0900

    12.4. 책 검색 컨트롤러 메소드 추가

--- project/src/main/java/sample/spring/yse/BookController.java
+++ project/src/main/java/sample/spring/yse/BookController.java
@@ -93,6 +93,11 @@ public class BookController {
 	    
 	    ModelAndView mav = new ModelAndView();
 	    mav.addObject("data", list);		
+	    
+	    if (map.containsKey("keyword")) {
+	        mav.addObject("keyword", map.get("keyword"));
+	    }
+	    
 	    mav.setViewName("/book/list");
 	    return mav;
 	}
